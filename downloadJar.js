c0b4eef2118d6ce42512a341ca25d8e9844533ef
Dominic Cobo <contact@dominiccobo.com>
2020-01-24 11:25:53 +0000

    Update server version

--- downloadJar.js
+++ downloadJar.js
@@ -9,7 +9,7 @@ if(azureApiKey === undefined) {
     process.exit(-1);
 }
 
-const serverVersion = "0.5.0";
+const serverVersion = "0.6.0";
 
 let repositoryEnvVarKey = 'PROJECT_MAVEN_REPOSITORY';
 const repositoryUrl = process.env[repositoryEnvVarKey];
